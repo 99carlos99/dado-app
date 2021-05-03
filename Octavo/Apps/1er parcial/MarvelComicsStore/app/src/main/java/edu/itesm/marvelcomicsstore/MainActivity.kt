d2ca933fbb8dd8e3ee139057b59964d2198917fa
99carlos99 <A01339011@itesm.mx>
2021-05-03 15:07:47 -0500

    First commit!

---
+++ Octavo/Apps/1er parcial/MarvelComicsStore/app/src/main/java/edu/itesm/marvelcomicsstore/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.itesm.marvelcomicsstore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
